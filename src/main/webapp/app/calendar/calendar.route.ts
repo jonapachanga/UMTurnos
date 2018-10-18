fa0b9dffbd8bd1eca02a52366dd9645d1e4ae513
Jonatan Eseiza <you@example.com>
2018-10-18 15:43:47 -0300

    se implemento tiny

---
+++ src/main/webapp/app/calendar/calendar.route.ts
@@ -0,0 +1,14 @@
+import { Route } from '@angular/router';
+
+import { UserRouteAccessService } from 'app/core';
+import { CalendarComponent } from './';
+
+export const CALENDAR_ROUTE: Route = {
+  path: 'calendar',
+  component: CalendarComponent,
+  data: {
+    authorities: [],
+    pageTitle: 'calendar.title'
+  },
+  canActivate: [UserRouteAccessService]
+};
