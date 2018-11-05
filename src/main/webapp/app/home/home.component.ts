2c44ec8b8e2de69475902e0152606dbb413ae252
matias <you@example.com>
2018-11-05 19:41:23 -0300

    se implemento full calendar

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
 
 import { LoginModalService, Principal, Account } from 'app/core';
+import { CalendarComponent } from 'ng-fullcalendar';
+import { Options } from 'fullcalendar';
+import { EventsService } from './events.service';
 
 @Component({
     selector: 'jhi-home',
@@ -10,16 +13,38 @@ import { LoginModalService, Principal, Account } from 'app/core';
     styleUrls: ['home.css']
 })
 export class HomeComponent implements OnInit {
+    calendarOptions: Options;
+    @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
     account: Account;
     modalRef: NgbModalRef;
+    events: object;
 
-    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {}
+    constructor(
+        private principal: Principal,
+        private loginModalService: LoginModalService,
+        private eventManager: JhiEventManager,
+        private eventsService: EventsService
+    ) {}
 
     ngOnInit() {
         this.principal.identity().then(account => {
             this.account = account;
         });
         this.registerAuthenticationSuccess();
+
+        this.eventsService.getEvents().subscribe(data => {
+            this.calendarOptions = {
+                editable: true,
+                eventLimit: false,
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay,listMonth'
+                },
+                selectable: true,
+                events: []
+            };
+        });
     }
 
     registerAuthenticationSuccess() {
@@ -36,5 +61,15 @@ export class HomeComponent implements OnInit {
 
     login() {
         this.modalRef = this.loginModalService.open();
+    }
+
+    clearEvents() {
+        this.events = [];
+    }
+
+    loadEvents() {
+        this.eventsService.getEvents().subscribe(data => {
+            this.events = data;
+        });
     }
 }
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -2,12 +2,13 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { EditorModule } from '@tinymce/tinymce-angular';
+import { FullCalendarModule } from 'ng-fullcalendar';
 
 import { UmturnosSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
 
 @NgModule({
-    imports: [UmturnosSharedModule, RouterModule.forChild([HOME_ROUTE]), EditorModule],
+    imports: [UmturnosSharedModule, RouterModule.forChild([HOME_ROUTE]), EditorModule, FullCalendarModule],
     declarations: [HomeComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
